0dc644c140904714dc9a98dcb87bb4da8642caa0
proxypain <53108551+proxypain@users.noreply.github.com>
2022-01-29 20:01:36 -0500

    Create game.py

---
+++ game.py
@@ -0,0 +1,23 @@
+import random
+
+number = random.randint(1, 999)
+
+playerName = input('What is your name? ')
+
+print('Hello', playerName)
+number_of_guesses = 0
+print('Lets play a game shordieee only 3 tries, pick a number 1 thru 999')
+
+while number_of_guesses < 5:
+    guess = int(input())
+    number_of_guesses += 1
+    if guess < number:
+        print('ahh too low damn try again lol' )
+    if guess > number:    
+        print('ahh too high damn try again lol')
+    if guess == number:
+        break
+if guess == number:
+    print('haha smart guy')
+else:
+    print('just go run it back, u trippin :[')
